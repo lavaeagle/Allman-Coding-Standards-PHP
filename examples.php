d453e697c9e0237dca2cba4ba51e0b7761f72936
Michael Calkins <michael@everyonecanweb.com>
2012-07-13 22:18:21 -0700

    Added control structures

---
+++ examples.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+*   Allman Coding Standards
+*
+*   Due to the spacing of the code
+*   this becomes a highly readable coding
+*   standard because there are fewer characters
+*   per line.
+*
+*/
+
+while($row = mysql_fetch_assoc($sql))
+{
+    
+}
+
+if($a == $b)
+{
+    
+}
+
+if($b == $c)
+{
+ 
+}
+elseif($b == $a)
+{
+ 
+}
+else
+{
+    
+}
+
+switch($type)
+{
+    case 'image' :
+        
+    break;
+    
+    default:
+        
+    break;
+}
+
+?>
